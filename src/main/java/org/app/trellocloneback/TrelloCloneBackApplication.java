2374ca267fa83743f589c1940b46d33030e1971a
Issei0813star <issei0813star.dev@gmail.com>
2024-01-20 00:04:43 +0900

    first commit

---
+++ src/main/java/org/app/trellocloneback/TrelloCloneBackApplication.java
@@ -0,0 +1,13 @@
+package org.app.trellocloneback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloCloneBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/app/trellocloneback/TrelloCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.trellocloneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
